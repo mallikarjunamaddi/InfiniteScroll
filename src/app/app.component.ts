8f7f45ce731e44b275b6390f6ae349678e0b1665
Mallikarjuna Maddi <rammallikarjunarao@gmail.com>
2020-11-12 23:23:36 +0530

    Integrated spinner and scroller in the UI.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 import { DataService } from './data.service';
 import { ImageString } from './models/ImageString';
@@ -11,29 +12,35 @@ import { ImageString } from './models/ImageString';
 export class AppComponent {
 
   image: string;
-  pageSize: number = 6;
+  pageSize: number = 9;
   imageList:string[] = [];
   imageStrings: ImageString[] = [];
+  isDataAvailable: boolean = true;
 
-  constructor(private dataService: DataService) {
+  constructor(private dataService: DataService, private spinner: NgxSpinnerService) {
     this.getImageStrings(1, this.pageSize);
   }
 
+  // To Fetch image's names
   getImageStrings(pageIndex: number, pageSize: number) {
     this.dataService.getImageStrings(pageIndex, pageSize).subscribe((data) => {
         try {
-          if(data) {
+          if(data.length > 0) {
             this.imageStrings = this.imageStrings.concat(data);
             this.imageStrings.forEach(imageString => {
               this.getImage(imageString.strValue)
             });
-          } 
+            this.spinner.hide();
+          } else {
+            this.isDataAvailable = false;
+          }
         } catch(error) {
             console.error(error);
         }
     });
   }
 
+  // To fetch image string (in base64 format)
   getImage(imageName: string) {
     this.dataService.getImage(imageName).subscribe((image) => {
       try {
@@ -45,5 +52,15 @@ export class AppComponent {
       }
     });
   }
-  
+
+  onScroll() {
+    // if data is avialable in last iteration
+    // then check for further data.
+    if(this.isDataAvailable) {
+      this.spinner.show();
+      let index = this.imageStrings.length / this.pageSize + 1;
+      console.log("Page Index:", index, "list Length", this.imageStrings.length)
+      this.getImageStrings(index, this.pageSize);
+    }
+  }
 }
